4c8c9a1fe8e429284898389ae00d077077652915
aj jaber <aljaberlax@gmail.com>
2022-04-18 21:40:16 +0600

    toast added

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,47 +1,53 @@
 import React from 'react';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../firebase.init';
 import './Register.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
+    const [sendEmailVerification, sending] = useSendEmailVerification(auth);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
     const navigate = useNavigate();
 
-    const navigateLogin = () =>{
+
+    const navigateLogin = () => {
         navigate('/login');
     }
 
-    if(user){
+    if (user) {
         navigate('/home');
     }
 
-    const handleRegister = event =>{
+    const handleRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
 
         createUserWithEmailAndPassword(email, password);
+        toast('Email Verification send')
     }
 
     return (
         <div className='register-form'>
-            <h2 className='text-primary pe-auto text-decoration-none' style={{textAlign: 'center'}}>Please Register</h2>
+            <h2 className='text-primary pe-auto text-decoration-none' style={{ textAlign: 'center', marginTop: '20px' }}>Please Register</h2>
             <form onSubmit={handleRegister}>
-                <input type="text" name="name" id="" placeholder='Your Name'/>
-                
-                <input type="email" name="email" id="" placeholder='Email Address' required/>
-                
-                <input type="password" name="password" id="" placeholder='Password' required/>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
                 <input className='btn btn-primary text-white pe-auto text-decoration-none' type="submit" value="Register" />
             </form>
             <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+            <ToastContainer />
         </div>
     );
 };
